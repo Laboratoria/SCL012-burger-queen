ea60458ae7964ee022e533ffc4b1a93e420a9af9
Camirojasserantoni <camilarojasserantoni@gmail.com>
2020-03-03 09:36:03 -0300

    subiendo cambio de status de antiguas modificaciones

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import hamburgesa from '../src/imagenes/830aa5f2008eff989e43d31ff30b7da7.jpg';
 import Button from './components/navegation.js';
 
 
 function App() {
   return (
     <div className="Log">
-    <img src={hamburgesa} alt="logo"/>
   
     <div className='App'>
       <Button />
--- src/components/navegation.js
+++ src/components/navegation.js
@@ -3,9 +3,9 @@ import React from 'react';
 
 function Button(props) {
   return   <header className='App-header'>
-    <div>
+    <div className="btnInit">
     <button type="button" className="btn btn-success btn-lg">Garzón</button>
-<button type="button" className="btn btn-danger btn-lg">Cocina</button>
+    <button type="button" className="btn btn-danger btn-lg">Cocina</button>
     </div>
 
   </header>;
