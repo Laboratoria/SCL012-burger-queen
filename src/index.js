0d73f3220f40eae27266f99f882891849b7c26be
Camirojasserantoni <camilarojasserantoni@gmail.com>
2020-03-03 11:13:12 -0300

    subiendo para bajar

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Men from './App';
-import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />,<Men/>, document.getElementById('root'));
